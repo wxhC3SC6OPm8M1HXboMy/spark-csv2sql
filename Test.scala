107bfdc0bca1b15361f7f69c030b8a0ac85ba118
Diego Klabjan <dklabjan@hotmail.com>
2015-03-20 08:36:36 -0500

    Create Test.scala

---
+++ Test.scala
@@ -0,0 +1,32 @@
+/**
+ * Created by diego on 11/21/14.
+ */
+
+import org.apache.spark.{SparkContext,SparkConf}
+
+import ReadCsv
+
+/*
+main code
+ */
+
+object Test {
+
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf().setAppName("onvia").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+    /*
+    load csv files as tables
+     */
+
+    params.Files.File.values.foreach(v => {
+      ReadCsv.readCsv(v,sc,sqlContext)
+    })
+    
+    sc.stop()
+  }
+}
